bf537bf3b2b23f847f441a60b228ae123afecea9
kikuishimoto <kikuish@gmail.com>
2020-08-28 19:15:04 -0700

    Changed params and added serializer

--- app/controllers/dogs_controller.rb
+++ app/controllers/dogs_controller.rb
@@ -1,21 +1,24 @@
 class DogsController < ApplicationController
     def index
         dogs = Dog.all 
-        render json: dogs
+        render json: DogSerializer.new(dogs)
     end 
     def create
         dog = Dog.new(dog_params)
         breed = Breed.find_or_create_by(name: params[:breed])
         dog.breed = breed
-        dog.save
-        render json: dog
+        if dog.save
+            render json: DogSerializer.new(dog)
+        else 
+            render json: {error: 'could not be created'}
+        end 
     end
 
 
 
     private 
     def dog_params
-        params.require(:dog).permit(:name, :age, :image_url, :description, :location)
+        params.require(:dog).permit(:name, :age, :image_url, :description, :location, :email)
     end
     def set_breed
         @breed = Breed.find_by(id: params[:id])
