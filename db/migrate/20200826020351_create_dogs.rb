6ccff25053e6a34e0e8e82e79615dc3c8d7cee87
kikuishimoto <kikuish@gmail.com>
2020-08-28 19:16:02 -0700

    added email and changed age to float

--- db/migrate/20200826020351_create_dogs.rb
+++ db/migrate/20200826020351_create_dogs.rb
@@ -3,9 +3,10 @@ class CreateDogs < ActiveRecord::Migration[6.0]
     create_table :dogs do |t|
       t.string :name
       t.belongs_to :breed
-      t.integer :age
+      t.float :age
       t.string :location
       t.text :description
+      t.text :email
       t.string :image_url
 
       t.timestamps
