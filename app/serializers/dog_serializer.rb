c6d06a627985c294e21df91a9c63da13335d21fd
kikuishimoto <kikuish@gmail.com>
2020-08-28 19:15:38 -0700

    added email to serializer

--- app/serializers/dog_serializer.rb
+++ app/serializers/dog_serializer.rb
@@ -1,4 +1,4 @@
 class DogSerializer
   include FastJsonapi::ObjectSerializer
-  attributes :name, :age, :location, :description, :image_url
+  attributes :name, :age, :location, :description, :email, :image_url
 end
