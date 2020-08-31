2b085dad587ba09804114a53c63545a9af0ff9ec
kikuishimoto <kikuish@gmail.com>
2020-08-31 14:55:03 -0700

    added show patch and destroy

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,5 @@
 Rails.application.routes.draw do
-  resources :dogs, only: [:create, :index]
+  resources :dogs, only: [:create, :index, :destroy, :show, :update]
   resources :breeds, only: [:create, :index]
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
