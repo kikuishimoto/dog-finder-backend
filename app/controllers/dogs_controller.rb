eaddb3d0eaa8b2ddf6b6f20c3c05fb31e92e9ffb
kikuishimoto <kikuish@gmail.com>
2020-08-31 14:54:14 -0700

    added show

--- app/controllers/dogs_controller.rb
+++ app/controllers/dogs_controller.rb
@@ -3,6 +3,10 @@ class DogsController < ApplicationController
         dogs = Dog.all 
         render json: DogSerializer.new(dogs)
     end 
+    def show
+        dog = Dog.find(params[:id])
+        render json: DogSerializer.new(dog)
+    end 
     def create
         dog = Dog.new(dog_params)
         breed = Breed.find_or_create_by(name: params[:breed])
@@ -13,6 +17,17 @@ class DogsController < ApplicationController
             render json: {error: 'could not be created'}
         end 
     end
+    def destroy
+        dog = Dog.find(params[:id])
+        dog.destroy
+        render json: {message: "Successfully deleted #{dog.name}!"}
+    end 
+    def update
+        dog = Dog.find(params[:id])
+        dog.update(dog_params)
+        render json: DogSerializer.new(dog)
+    end 
+
 
 
 
